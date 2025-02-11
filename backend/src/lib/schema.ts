30be11bea76c0a3ec3c6cad4c080fb433d034651
zrotrasukha <zrotrasukha@gmail.com>
2025-02-11 11:34:27 +0530

    Account Schema

--- backend/src/lib/schema.ts
+++ backend/src/lib/schema.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
@@ -22,6 +22,22 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  token: {
+    type: String,
+  },
+});
+
+const accountSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
 });
 
 export const User = model("User", userSchema);
+export const Account = model("Account", accountSchema);
