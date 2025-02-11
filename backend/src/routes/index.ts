c1584a5b0842e3085c9f0cc5ccbbf5385c1705fb
zrotrasukha <zrotrasukha@gmail.com>
2025-02-11 23:51:24 +0530

    added account router

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,7 @@
-import express from "express";
+import express, { Response } from "express";
 import { router as userRouter } from "./user";
+import { router as accountRouter } from "./account";
 
 export const router = express.Router();
 router.use("/user", userRouter);
+router.use("/account", accountRouter);
