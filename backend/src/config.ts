a401e5e0f3cc2d9bc04b758b54a3c03237809558
zrotrasukha <zrotrasukha@gmail.com>
2025-02-13 19:06:04 +0530

    addedcheck for jwt availability

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,1 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export const jwt_secret = process.env.JWT_SECRET as string;
+export const MONGO_URI = process.env.MONGODB_URI as string;
+
+if (!jwt_secret || !MONGO_URI) {
+  throw new Error("Missing environment variables! Check your .env file");
+}
