a0f9d595c246a9c919786c736df3234c7ca23e0e
zrotrasukha <zrotrasukha@gmail.com>
2025-02-13 19:06:26 +0530

    modified

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
 import { router as rootRouter } from "./routes/index";
+import dbConnect from "./lib/db";
 
+dbConnect();
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -9,7 +11,7 @@ app.use(express.json());
 const PORT = 3000;
 
 app.use("/api/v1", rootRouter);
-app.use""
+
 app.listen(PORT, () => {
   console.log("the site is live now at port 3000");
 });
