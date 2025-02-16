0932089388958bb5006a45f5b793f32f0d3e32f6
zrotrasukha <zrotrasukha@gmail.com>
2025-02-16 18:45:28 +0530

    added routers and toaster

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { SignupPage } from "./pages/signupPage.tsx";
+import SigninPage from "./pages/signinPage.tsx";
+import Dashboard from "./pages/dashboardPage.tsx";
+import Sendpage from "./pages/sendPage.tsx";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Toaster/>
+      <Routes>
+        <Route index path="/" element={<App />} />
+        <Route path="/signin" element={<SigninPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<Sendpage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
