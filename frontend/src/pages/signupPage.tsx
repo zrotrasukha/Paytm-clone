f3a20fd81a174e2de7709b1d0238f631907b5f4a
zrotrasukha <zrotrasukha@gmail.com>
2025-02-16 23:36:03 +0530

    removed headless from toast import, added axios request and toast notification in trycatch block

--- frontend/src/pages/signupPage.tsx
+++ frontend/src/pages/signupPage.tsx
@@ -6,30 +6,35 @@ import { PasswordProvider } from "../context/context";
 import { PasswordInput } from "../components/passwordInput";
 import axios from "axios";
 import { useNavigate } from "react-router";
-import toast from "react-hot-toast/headless";
+import toast from "react-hot-toast";
 
 export const SignupPage = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [Email, setEmail] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [passwordToggle, setPasswordToggle] = useState(false);
+  const [passwordToggle, setPasswordToggle] = useState(true);
   const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const response = await axios.post(
-      "http://localhost:3000/api/v1/user/signup",
-      { firstName, lastName, Email, password },
-    );
 
-    if (response.status === 200) {
-      console.log(response.data);
-      toast.success("User created successfully");
-      navigate("/login");
-    } else {
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/v1/user/signup",
+        { firstName, lastName, email, password },
+      );
+
+      if (response.status === 200) {
+        console.log(response.data);
+        toast.success("User created successfully");
+        navigate("/signin");
+        return;
+      }
+      throw new Error();
+    } catch (error) {
+      console.error(error);
       toast.error("User already exists");
-      throw new Error("User already exists");
     }
   };
   return (
@@ -68,7 +73,7 @@ export const SignupPage = () => {
               <label className="text-md font-bold">Email</label>
               <input
                 type="text"
-                value={Email}
+                value={email}
                 placeholder="johndoe@email.com"
                 className=" outline-none focus:border-zinc-500 p-2 text-sm text-gray-700 font-semibold border-2 w-full border-gray-300 rounded-md mt-1"
                 onChange={(e) => setEmail(e.target.value)}
