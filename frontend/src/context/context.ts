6bb3e209173a2be4afab2c00c585fb77bbc52898
zrotrasukha <zrotrasukha@gmail.com>
2025-02-18 17:22:41 +0530

    changed password context to appContext

--- frontend/src/context/context.ts
+++ frontend/src/context/context.ts
@@ -1,20 +1,28 @@
 import { useContext, createContext } from "react";
 
-export type PasswordContextType = {
+export type AppContextType = {
   password: string;
   setPassword: React.Dispatch<React.SetStateAction<string>>;
   passwordToggle: boolean;
   setPasswordToggle: React.Dispatch<React.SetStateAction<boolean>>;
+  firstName: string;
+  setFirstName: React.Dispatch<React.SetStateAction<string>>;
+  lastName: string;
+  setLastName: React.Dispatch<React.SetStateAction<string>>;
+  email: string;
+  setEmail: React.Dispatch<React.SetStateAction<string>>;
 };
 
-const PasswordContext = createContext<PasswordContextType | null>(null);
+const AppContext = createContext<AppContextType | null>(null);
 
-export const usePasswordContext = () => {
-  const context = useContext(PasswordContext);
+export const useAppContext = () => {
+  const context = useContext(AppContext);
   if (!context) {
-    throw new Error("usePasswordContext must be used within a PasswordContextProvider");
+    throw new Error(
+      "usePasswordContext must be used within a PasswordContextProvider",
+    );
   }
   return context;
 };
 
-export const PasswordProvider = PasswordContext.Provider;
+export const ContextProvider = AppContext.Provider;
