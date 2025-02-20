ce15ed700da44b6611458ad6b7fabb3cb15def52
zrotrasukha <zrotrasukha@gmail.com>
2025-02-21 00:42:40 +0530

    added className prop

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -4,14 +4,17 @@ import Subheading from "./subheading";
 export default function Headings({
   heading,
   subheading,
+  className
 }: {
   heading: string;
   subheading: string;
+  className?: string;
 }) {
   return (
-    <>
+  <div className={`flex flex-col ${className}`}>
+
       <Heading heading={heading} />
       <Subheading children={subheading} className="p-2" />
-    </>
+  </div>
   );
 }
