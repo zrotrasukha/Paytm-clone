cc51907242f8fe235844f004bb13706228fdd16a
zrotrasukha <zrotrasukha@gmail.com>
2025-02-21 00:39:27 +0530

    added onClick and disabled in props

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -3,13 +3,16 @@ type props = {
   classname?: string;
   children?: React.ReactNode;
   type?: "button" | "submit" | "reset";
-  onClick?: () => void;
+  onClick?:React.MouseEventHandler<HTMLButtonElement> | undefined ;
+  disabled?: boolean;
 };
-export default function Button({ classname, children, type, onClick}: props) {
+export default function Button({ classname, children, type, onClick, disabled }: props) {
   return (
     <button
-      className={` w-fit px-2 bg-black h-10 rounded-md font-bold text-md ${classname} `} type={type} onClick={onClick}
-
+      className={` w-fit disabled:bg-neutral-700 px-2 bg-black h-10 rounded-md font-bold text-md ${classname}`}
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
